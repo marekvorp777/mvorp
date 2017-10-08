a5893d53f0f3db01fa3b11eeb2c0d3bd8d21387f
marekvorp777 <marek.vorp@gmail.com>
2017-10-09 02:18:17 +0300

    Task 4123

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,21 +1,20 @@
 package ru.job4j;
 
 /**
-*Class to write in console a simple sentence 'Hello world.'
+*Class to write in console a simple sentence 'Hello world.'.
 *
 *@author mvorp
 *@since 09.10.2017
 *@version 1
 */
-public class Calculate{
+public class Calculate {
 	/**
      * Main.
      * @param args - args.
      */
-	public static void main(String[] args){
-		System.out.println("Hello world.");			
+	public static void main(String[] args) {
+		System.out.println("Hello world.");
 	}
-	
 	/**
 	* Method echo.
 	* @param name Your name.
--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -1,34 +1,32 @@
 package ru.job4j;
 
 import org.junit.Test;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-*Class to test 'Calculate.java'
+*Class to test 'Calculate.java'.
 *
 *@author Marek Vorp (mailto:marek.vorp@gmail.com)
 *@version $Id$
 *@since 09.10.2017
 */
-public class CalculateTest{
-	
+public class CalculateTest {
 	/**
 	* Test echo.
-	*/ 
+	*
+	*@author Marek Vorp (mailto:marek.vorp@gmail.com)
+	*@version $Id$
+	*@since 09.10.2017
+	*/
 	@Test
-	public void whenTakeNameThenTreeEchoPlusName(){
+	public void whenTakeNameThenTreeEchoPlusName() {
 		String input = "Marek Vorp";
 		String expect = "Echo, echo, echo:Marek Vorp";
-	
 		//Создание нового объекта.
 		Calculate calc = new Calculate();
-	
 		//Выполнение метода echo с параметром input и запись ее в переменную result.
-		String result = calc.echo(input); 
-	
+		String result = calc.echo(input);
 		//Проверка, что метод возвращает 'result' ожидаемый результат 'expect'
 		assertThat(result, is(expect));
 	}
