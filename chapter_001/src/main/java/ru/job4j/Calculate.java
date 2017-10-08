dd5cec843f4da1b9567e9cd68305328f12d935e1
marekvorp777 <marek.vorp@gmail.com>
2017-10-09 00:41:01 +0300

    Task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,12 @@
 package ru.job4j;
 
+/**
+*Class to write in console a simple sentence 'Hello world.'
+*
+*@author mvorp
+*@since 09.10.2017
+*@version 1
+*/
 public class Calculate{
 	public static void main(String[] args){
 		System.out.println("Hello world.");	
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Marek Vorp (mailto:marek.vorp@gmail.com)
+* @version $Id$
+* @since 09.10.2017
+*/
+package ru.job4j;
