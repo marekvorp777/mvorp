891687bf0346e216f6438ae1392a3e8b2082a134
marekvorp777 <marek.vorp@gmail.com>
2017-10-09 01:40:52 +0300

    Task 4122

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -8,8 +8,21 @@ package ru.job4j;
 *@version 1
 */
 public class Calculate{
+	/**
+     * Main.
+     * @param args - args.
+     */
 	public static void main(String[] args){
-		System.out.println("Hello world.");	
+		System.out.println("Hello world.");			
+	}
+	
+	/**
+	* Method echo.
+	* @param name Your name.
+	* @return Echo plus your name.
+	*/
+	public String echo(String name) {
+		return "Echo, echo, echo:" + name;
 	}
 }
 
